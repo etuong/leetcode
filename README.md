6f6ac6c1bca29cbb6058d2d9247f866f3e00cd56
etuong <etuong@gmail.com>
2024-12-07 15:08:10 -0800

    Yay I solved my first problem

---
+++ README.md
@@ -0,0 +1,9 @@
+# LeetCode
+
+### LeetCode Algorithm
+
+(Notes: "🔒" means you need to buy a book from Leetcode)
+
+| #   | Title                                             | Solution                         | Difficulty |
+| --- | ------------------------------------------------- | -------------------------------- | ---------- |
+| 1   | [Two Sum](https://leetcode.com/problems/two-sum/) | [Python](./algorithms/TwoSum.py) | Easy       |
---
+++ algorithm/TwoSum.py
@@ -0,0 +1,37 @@
+from typing import List
+
+class Solution:
+    def twoSumBrtuteForce(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums) - 1):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return i, j
+
+    def twoSumSorted(self, nums: List[int], target: int) -> List[int]:
+        sorted_nums  = sorted(enumerate(nums), key=lambda x: x[1])
+        left = 0
+        right = len(nums) - 1
+        while right >= left:
+            current_sum = sorted_nums [left][1] + sorted_nums [right][1]
+            if current_sum == target:
+                return sorted_nums [left][0], sorted_nums [right][0]
+            if current_sum > target:
+                right = right - 1
+            if current_sum < target:
+                left = left + 1
+
+    def twoSumBest(self, nums: List[int], target: int) -> List[int]:
+        hash_map = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hash_map:
+                return hash_map[complement], i
+            hash_map[nums[i]] = i
+
+
+nums = [2,1,6,3,5,4]
+target = 11
+s = Solution()
+print(s.twoSumBrtuteForce(nums, target))
+print(s.twoSumSorted(nums, target))
+print(s.twoSumBest(nums, target))
